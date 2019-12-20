c74af16910a944383a5174db9c65e78b97ed3ece
Andy Doan <andy@foundries.io>
2019-12-20 13:36:05 -0600

    Pull service images when pulling down docker app
    This is only useful for a single node situation such as embedded
    devices.
    
    Signed-off-by: Andy Doan <andy@foundries.io>

--- internal/commands/pull.go
+++ internal/commands/pull.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"context"
 	"fmt"
 	"os"
 
@@ -11,6 +12,9 @@ import (
 	"github.com/docker/cli/cli/command"
 	"github.com/docker/cli/cli/config"
 	"github.com/docker/distribution/reference"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/pkg/jsonmessage"
+	"github.com/docker/docker/registry"
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
@@ -26,6 +30,34 @@ func pullCmd(dockerCli command.Cli) *cobra.Command {
 		},
 	}
 	return cmd
+}
+
+func pullImage(ctx context.Context, cli command.Cli, image string) error {
+	ref, err := reference.ParseNormalizedNamed(image)
+	if err != nil {
+		return err
+	}
+
+	// Resolve the Repository name from fqn to RepositoryInfo
+	repoInfo, err := registry.ParseRepositoryInfo(ref)
+	if err != nil {
+		return err
+	}
+	authConfig := command.ResolveAuthConfig(ctx, cli, repoInfo.Index)
+	encodedAuth, err := command.EncodeAuthToBase64(authConfig)
+	if err != nil {
+		return err
+	}
+	options := types.ImagePullOptions{
+		RegistryAuth: encodedAuth,
+	}
+	responseBody, err := cli.Client().ImagePull(ctx, image, options)
+	if err != nil {
+		return err
+	}
+	defer responseBody.Close()
+
+	return jsonmessage.DisplayJSONMessagesStream(responseBody, cli.Out(), cli.Out().FD(), false, nil)
 }
 
 func runPull(dockerCli command.Cli, name string) error {
@@ -51,6 +83,14 @@ func runPull(dockerCli command.Cli, name string) error {
 		return err
 	}
 	fmt.Fprintf(os.Stdout, "Successfully pulled %q (%s) from %s\n", bndl.Name, bndl.Version, ref.String())
+
+	ctx := context.Background()
+	for name, image := range bndl.Images {
+		fmt.Fprintf(os.Stdout, "Pulling: %s -> %s\n", name, image.Image)
+		if err := pullImage(ctx, dockerCli, image.Image); err != nil {
+			return err
+		}
+	}
 
 	return nil
 }
